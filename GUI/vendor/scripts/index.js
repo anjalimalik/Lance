11cd0ee28db2c7ab683090800f57e5275bffcca3
Shengqi <shengqiw97@gmail.com>
2018-02-22 12:10:54 -0500

    js for new UI

--- GUI/vendor/scripts/index.js
+++ GUI/vendor/scripts/index.js
@@ -26,7 +26,7 @@ function btn_login() {
 
                             this.authToken = data.authToken
                             this.email = _email;
-                            
+
 
 			            }.bind(this));
 			        }
@@ -50,10 +50,6 @@ function btn_register() {
     var _cpass = inputConfirmPws.value;
     var _agree = ch.checked;
 
-    if (!_agree) {
-        alert("Must agree to the terms and conditions!");
-        return;
-    }
 
     if (_pass !== _cpass || _pass.length == 0) {
         alert("The two passwords do not match, try again!");
@@ -103,15 +99,35 @@ function btn_register() {
         }
     }
 
+function btn_register_continue () {
+    var email = in_register_email.value;
+    var pass = in_register_pass.value;
+    var fName = in_register_fName.value;
+    var lName = in_register_lName.checked;
 
+
+    if (!verifyEmail(email)) {
+        return;
+    }
+    else {
+        $('#myModal2').modal('hide');
+        $("#myModal3").modal();
+    }
+
+//setAttribute("data-target", "myModal3")
+
+}
 
 function verifyEmail(_email) {
-    
+
     var idx = _email.indexOf("@purdue.edu");
     if (idx == -1 || idx != _email.length-11 || idx == 0){
-        document.getElementById("emailFeedback").style.visibility = "visible";
+        document.getElementById("emailFeedback").value = "Invalid Email";
         return false;
     }
+
+    document.getElementById("emailFeedback").visibility = "visible";
+
     return true;
 
 }
