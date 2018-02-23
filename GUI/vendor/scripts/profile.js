1e6c483e0d40a4c0d7eba79032b8d4a8bb810024
Shengqi <shengqiw97@gmail.com>
2018-02-23 05:03:19 -0500

    added more stuff to profile creation

--- GUI/vendor/scripts/profile.js
+++ GUI/vendor/scripts/profile.js
@@ -1,21 +1,25 @@
 
-var email, pass, fName, lName, edu, skills, desc, contact, links, pic, docs;
+var email, pass, name, edu, skills, desc, contact, links, pic, docs;
 
 function body_onload() {
 
+    name = localStorage.getItem('name');
+    email = localStorage.getItem('email');
     edu = localStorage.getItem('edu');
     links = localStorage.getItem('links');
     contact = localStorage.getItem('contact');
     desc = localStorage.getItem('desc');
     skills = localStorage.getItem('skills');
 
+    document.getElementById("profile_name").innerHTML = name;
+    document.getElementById("profile_email").innerHTML = email;
     document.getElementById("profile_edu").innerHTML = edu;
     document.getElementById("profile_contact").innerHTML = contact;
     document.getElementById("profile_desc").innerHTML = desc;
     document.getElementById("profile_skills").innerHTML = skills;
 
     links = "https://" + links;
-    var webLink = "<a href='" + links + "' id=\"profile_links\" class=\"card-link\">Link</a>"
+    var webLink = "<a href='" + links + "' id=\"profile_links\" class=\"card-link\">Portfolio Link</a>"
     document.getElementById("profile_links").innerHTML = webLink;
 
 }
