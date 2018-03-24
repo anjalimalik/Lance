41049e341bf7300a92cffb00e3ba28181bf8a825
Anjali Malik <anjali30malik@gmail.com>
2018-03-24 02:13:30 -0400

    like/interested button

--- GUI/vendor/scripts/home.js
+++ GUI/vendor/scripts/home.js
@@ -25,6 +25,7 @@ function getAllPosts() {
         });
 }
 
+// CREATE A NEW CARD FOR EVERY POST FROM SERVER (/getPosts)
 function createCard(user, content, headline, postingType, price, postID) {
 
     var ul = document.getElementById('news_card_list');
@@ -37,27 +38,32 @@ function createCard(user, content, headline, postingType, price, postID) {
     divCenter.setAttribute('class', 'card text-center');
     li.appendChild(divCenter);
 
+    /* Offer/Request */
     var divHeader = document.createElement("div");
     divHeader.setAttribute('class', 'card-header');
     var str = postingType.concat(" from ", user);
-    divHeader.style = "margin-left:50px;";
+    divHeader.style = "color:#666699; margin-left:60px;";
     divHeader.innerHTML = str;
     divCenter.appendChild(divHeader);
 
+    /* Price */
     var divTextPrice = document.createElement("kbd");
     divTextPrice.innerHTML = "$".concat(price);
-    divTextPrice.style = "float:right;margin-right:3px;";
+    divTextPrice.style = "background-color:lightgrey;color:black;float:right;margin-right:3px;font-size: 16px;";
     divHeader.appendChild(divTextPrice);
 
     var divBody = document.createElement("div");
     divBody.setAttribute('class', 'card-body');
     divCenter.appendChild(divBody);
 
+    /* Title */
     var divTitle = document.createElement("h5");
     divTitle.setAttribute('class', 'card-title');
     divTitle.innerHTML = headline;
+    divTitle.style = "color:black;";
     divBody.appendChild(divTitle);
 
+    /* DESCRIPTION */
     var divText = document.createElement("p");
     divText.setAttribute('class', 'card-text');
     divText.innerHTML = content;
@@ -67,36 +73,52 @@ function createCard(user, content, headline, postingType, price, postID) {
     divButtons.setAttribute('class', 'card-text');
     divCenter.appendChild(divButtons);
 
+    /* Close Post */
     var btn_close = document.createElement("BUTTON");
     var t2 = document.createTextNode("Close Post");
     btn_close.setAttribute("class", "btn btn-danger btn-sm");
     btn_close.setAttribute("id", "btnClose");
-    btn_close.style = "float:right; margin-bottom: 0px; margin-right:15px; margin-top:0px;";
+    btn_close.style = "float:right;margin-bottom:0px;margin-right:15px;margin-top:0px;";
     btn_close.appendChild(t2);
     divButtons.appendChild(btn_close);
 
+    /* Report */
     var btn_report = document.createElement("BUTTON");
     var t1 = document.createTextNode("Report Post");
     btn_report.setAttribute("id", "btnReport");
     btn_report.setAttribute("class", "btn btn-warning btn-sm");
     btn_report.setAttribute("data-toggle", "modal");
     btn_report.setAttribute("data-target", "#myModalReport");
-    btn_report.style = "float:right; margin-bottom: 0px; margin-right:10px; margin-top:0px;";
+    btn_report.style = "float:right;margin-bottom:0px;margin-right:10px;margin-top:0px;";
     btn_report.appendChild(t1);
     divButtons.appendChild(btn_report);
 
+    /* Learn More */
     var divLearnMore = document.createElement("BUTTON");
     divLearnMore.setAttribute('class', 'btn btn-primary btn-md');
-    divLearnMore.style = "float:right; margin-bottom: 7px; margin-right:380px; margin-top:0px;";
+    divLearnMore.style = "float:right;margin-bottom:7px;margin-right:380px;margin-top:0px;";
     divLearnMore.innerHTML = "Learn More!";
     divButtons.appendChild(divLearnMore);
 
+    /* Date Posted */
     var divFooterDate = document.createElement('div');
     divFooterDate.setAttribute('class', 'card-footer text-muted');
     divFooterDate.innerHTML = "some number";
     divCenter.appendChild(divFooterDate);    
+    
+    /* LIKE/INTERESTED */
+    var aHeart = document.createElement('BUTTON');
+    aHeart.setAttribute('class', 'btn btn-default btn-sm');
+    aHeart.style = "border: 0; background: transparent;";
+    divFooterDate.appendChild(aHeart);  
 
-    // This needs to be hidden - Post id
+    var spanHeart = document.createElement('img');
+    spanHeart.setAttribute('src', 'heart.png');
+    spanHeart.setAttribute('alt', 'Like');
+    spanHeart.style = "width:20px; height:20px;";
+    aHeart.appendChild(spanHeart); 
+
+    /* This needs to be hidden - Post id */
     var post_id = document.createElement("LABEL");
     post_id.setAttribute("id", "postIDHidden");
     post_id.style.display = "none";
