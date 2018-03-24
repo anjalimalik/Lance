a319a615676482bf43fd8b7ce836779d75b9b1df
Shengqi <shengqiw97@gmail.com>
2018-03-23 23:05:51 -0400

    Added report functionality

--- GUI/vendor/scripts/home.js
+++ GUI/vendor/scripts/home.js
@@ -1,4 +1,5 @@
 var urlGetPosts = "http://localhost:5500/getPosts"
+var urlReport = "http://localhost:5500/Report"
 
 function onLoad() {
     getAllPosts();
@@ -16,11 +17,9 @@ function getAllPosts() {
                     var json = data.response;
 
                     for (i = 0; i < numPost; i++) {
-                        createCard(json[i].UserID, json[i].Content, json[i].Headline, json[i].PostingType, json[i].money);
+                        createCard(json[i].UserName, json[i].Content, json[i].Headline, json[i].PostingType, json[i].money, json[i].idPosts);
                     }
-
                 });
-
 
             })
             .catch(function(data) {
@@ -29,7 +28,7 @@ function getAllPosts() {
         });
 }
 
-function createCard(user, content, headline, postingType, money) {
+function createCard(user, content, headline, postingType, price, postID) {
 
     var ul = document.getElementById('news_card_list');
 
@@ -61,9 +60,14 @@ function createCard(user, content, headline, postingType, money) {
     divText.innerHTML = content;
     divBody.appendChild(divText);
 
+    var divTextPrice = document.createElement("p");
+    divTextPrice.setAttribute('class', 'card-text');
+    divTextPrice.innerHTML = "$".concat(price);
+    divBody.appendChild(divTextPrice);
+
     var divLearnMore = document.createElement('a');
     divLearnMore.setAttribute('class', 'btn btn-primary');
-    divLearnMore.setAttribute('href', 'www.google.com');
+    divLearnMore.setAttribute('href', 'https://www.google.com');
     divLearnMore.innerHTML = "Learn More!";
     divBody.appendChild(divLearnMore);
 
@@ -72,9 +76,51 @@ function createCard(user, content, headline, postingType, money) {
     divFooterDate.innerHTML = "some number";
     divCenter.appendChild(divFooterDate);
 
-    var divFooterMoney = document.createElement('div');
-    divFooterMoney.setAttribute('class', 'card-footer text-muted');
-    divFooterMoney.innerHTML = money;
-    divCenter.appendChild(divFooterMoney);
+    var btn_report = document.createElement("BUTTON");
+    var t = document.createTextNode("Report Post");
+    btn_report.appendChild(t);
+    btn_report.onclick = reportPost;
+    divBody.appendChild(btn_report);
+
+    var post_id = document.createElement("LABEL");
+    post_id.setAttribute("id", "postIDHidden");
+    post_id.style.display = "none";
+
+    post_id.innerHTML = postID;
+    divBody.appendChild(post_id);
+
+}
+
+function reportPost() {
+    var reportMsg = "Profanity in post";
+    console.log(document.getElementById("postIDHidden").textContent);
+    fetch(urlReport, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "message":reportMsg,
+                    "postId": document.getElementById("postIDHidden").textContent
+                })
+
+            }).then(function(res) {
+                if (res.ok) {
+                    res.json().then(function(data) {
+                        console.log("Inside res.ok");
+                        alert("Your Report was sent successfully");
+                    }.bind(this));
+                }
+                else {
+                    alert("Error: Login unsuccessful!");
+                    res.json().then(function(data) {
+                    console.log(data.message);
+                    }.bind(this));
+                }
+            }).catch(function(err) {
+                alert("Error: No internet connection!");
+                console.log(err.message + ": No Internet Connection");
+        });
 
 }
