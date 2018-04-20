94a32850da639e94b74d203a5302bd5b6fc3bf5b
Anjali Malik <anjali30malik@gmail.com>
2018-04-20 01:28:48 -0400

    create list in JS; added timesince function

--- Js/notificationsPage.js
+++ Js/notificationsPage.js
@@ -48,45 +48,58 @@ function getAllNotifications(id) {
         if (res.ok) {
             res.json().then(function (data) {
                 console.log(data.message);
+                var json = data.response;
                 var length = Object.keys(data.response).length;
-                if (length != 0 && numNotifs < length) {
-                    numNotifs = 0;
-                    var json = data.response;
+
+                var list = document.getElementsById("nList");
+
+                if (length != 0) {
+
+                    // create a container div
+                    var listDiv = document.createElement("div");
+                    a.setAttribute('class', 'notificationsContainer');
+                    list.appendChild(listDiv);
+
+                    // create the list using loop
                     for (i = 0; i < length; i++) {
-                        var ul = document.createElement("a");
-                        ul.setAttribute('class', 'notifClass dropdown-item');
-                        ul.innerHTML = (json[i].Notification);
-                        ul.style = "border-bottom: 1px solid #ccc;color:#333399;";
-                        document.getElementById("notif").appendChild(ul);
+                        var a = document.createElement("a");
+                        a.setAttribute('class', 'list-group-item list-group-item-action flex-column align-items-start');
+                        a.setAttribute('href', '#');
+                        listDiv.appendChild(a);
 
-                        numNotifs++;
+                        var head = document.createElement("div");
+                        head.setAttribute('class', 'd-flex w-100 justify-content-between');
+                        a.appendChild(head);
+
+                        var notif = document.createElement("h4");
+                        notif.setAttribute('class', 'mb-1');
+                        notif.innerHTML = (json[i].Notification);
+                        head.appendChild(notif);
+
+                        var ago = document.createElement("small");
+                        ago.innerHTML = (timeSince(new Date(Date.now() - json[i].msec)));
+                        head.appendChild(ago);
+
+                        var p = document.createElement("p");
+                        p.setAttribute('class', 'mb-1');
+                        p.innerHTML = json[i].SenderName;
+                        a.appendChild(p);
                     }
-
-                    // view all notifications list
-                    var listNotifs = document.createElement("a");
-                    listNotifs.setAttribute('class', 'notifClass dropdown-item');
-                    listNotifs.innerHTML = "See All";
-                    var notificationsPage = "./notifications.html?".concat(uID);
-                    listNotifs.setAttribute('href', notificationsPage);
-                    listNotifs.style = "border-bottom: 1px solid #ccc; text-align:center; color:#333399; font-weight: bold;";
-                    document.getElementById("notif").appendChild(listNotifs);
                 }
                 else if (numNotifs == 0) {
-                    numNotifs--;
-                    var ul = document.createElement("a");
-                    ul.setAttribute('class', 'notifClass dropdown-item half-rule');
-                    ul.innerHTML = "No new notifications available for you at this time.";
-                    ul.style = "border-bottom: 1px solid #ccc; margin-left:0;";
-                    document.getElementById("notif").appendChild(ul);
+                    var a = document.createElement("a");
+                    a.setAttribute('class', 'list-group-item list-group-item-action flex-column align-items-start');
+                    a.setAttribute('href', '#');
+                    listDiv.appendChild(a);
 
-                    // view all notifications list
-                    var listNotifs = document.createElement("a");
-                    listNotifs.setAttribute('class', 'notifClass dropdown-item');
-                    listNotifs.innerHTML = "See All";
-                    var notificationsPage = "./notifications.html?".concat(uID);
-                    listNotifs.setAttribute('href', notificationsPage);
-                    listNotifs.style = "border-bottom: 1px solid #ccc; text-align:center; color:#333399; font-weight: bold;";
-                    document.getElementById("notif").appendChild(listNotifs);
+                    var head = document.createElement("div");
+                    head.setAttribute('class', 'd-flex w-100 justify-content-between');
+                    a.appendChild(head);
+
+                    var notif = document.createElement("h4");
+                    notif.setAttribute('class', 'mb-1');
+                    notif.innerHTML = "You have zero notifications!";
+                    head.appendChild(notif);
                 }
 
             }.bind(this));
@@ -104,9 +117,49 @@ function getAllNotifications(id) {
 }
 
 function backToHome() {
-
+    var u = 'home.html?email='.concat(em);
+    window.location.href = u;
 }
 
 function backToProfile() {
+    var u = 'profile.html?email='.concat(em);
+    window.location.href = u;
+}
+
+function timeSince(date) {
     
+      var seconds = Math.floor((new Date() - date) / 1000);
+    
+      // years
+      var interval = Math.floor(seconds / 31536000);
+      if (interval > 1) {
+        return interval + " years ago";
+      }
+
+      // months
+      interval = Math.floor(seconds / 2592000);
+      if (interval > 1) {
+        return interval + " months ago";
+      }
+
+      // days
+      interval = Math.floor(seconds / 86400);
+      if (interval > 1) {
+        return interval + " days ago";
+      }
+
+      // hours
+      interval = Math.floor(seconds / 3600);
+      if (interval > 1) {
+        return interval + " hours ago";
+      }
+
+      // minutes
+      interval = Math.floor(seconds / 60);
+      if (interval > 1) {
+        return interval + " minutes ago";
+      }
+
+      // else - seconds
+      return Math.floor(seconds) + " seconds ago";
 }
